4f1b7aa81648bf1701f316017ead33ed55515622
tj <tjyoon0324@gmail.com>
2023-03-11 12:16:40 +0900

    bible_list data added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+from bible_list import *
+
 bible_page = requests.get("https://www.bskorea.or.kr/bible/korbibReadpage.php?version=GAE&book=gen&chap=1&sec=1&cVersion=&fontSize=15px&fontWeight=normal")
 bible_page_text = bible_page.text
 
