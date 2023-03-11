abdbe0ebd0a4b66ea1c16d026984e3afb79243e8
tj <tjyoon0324@gmail.com>
2023-03-11 11:47:16 +0900

    'let's go'

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
-bible_page = requests.get("https://www.bskorea.or.kr/bible/korbibReadpage.php?version=GAE&book=gen&chap=60&sec=1&cVersion=&fontSize=15px&fontWeight=normal").text
+bible_page = requests.get("https://www.bskorea.or.kr/bible/korbibReadpage.php?version=GAE&book=gen&chap=1&sec=1&cVersion=&fontSize=15px&fontWeight=normal")
+bible_page_text = bible_page.text
 
-print(bible_page)
+soup = BeautifulSoup(bible_page.text, 'html.parser')
+
+option_tags = soup.select(".searchList li:nth-of-type(2) > ul > li:nth-of-type(2) select option")
+
+print(bible_page_text)
+# print(option_tags)
