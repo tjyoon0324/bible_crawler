58954784748f90af479266e485bffafcfc037e19
tj <tjyoon0324@gmail.com>
2023-03-14 17:48:13 +0900

    code working. directory and file name formatting needs to be completed.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from bible_list import szGAEBook
 bible_title_chapter_list = szGAEBook
 
 for i in bible_title_chapter_list:
-    os.makedirs('/Users/tj/PycharmProjects/bible_crawler/bible_text/{}'.format(i[0]))
-    for j in range(2,len(i)):
-        url = "https://www.bskorea.or.kr/bible/korbibReadpage.php?version=GAE&book={}&chap={}&sec=1&cVersion=&fontSize=15px&fontWeight=normal".format(i[1],i[j])
+    os.makedirs('/Users/tj/PycharmProjects/bible_crawler/bible_text/{}. {}'.format(i[0],i[1]))
+    for j in range(3,len(i)):
+        url = "https://www.bskorea.or.kr/bible/korbibReadpage.php?version=GAE&book={}&chap={}&sec=1&cVersion=&fontSize=15px&fontWeight=normal".format(i[2],i[j])
         # url = "https://www.bskorea.or.kr/bible/korbibReadpage.php?version=GAE&book={}&chap={}&sec=1&cVersion=&fontSize=15px&fontWeight=normal".format(bible_title_chapter_list[0][1],bible_title_chapter_list[0][i])
         bible_page = requests.get(url)
         bible_page_text = bible_page.text
@@ -31,21 +31,27 @@ for i in bible_title_chapter_list:
 
         result = []
 
-        for n in range(0,len(pre_result)):
-            for k in range(0,len(pre_result[n])-2):
-                if (pre_result[n][k] == ',') and (pre_result[n][k+2].isnumeric()):
-                    temp_list = list(pre_result[n])
-                    temp_list.pop(k)
-                    temp_list.insert(k,'\n')
-                    temp_string = ''.join(temp_list)
-                    splitted = temp_string.split('\n ')
-                    print(splitted)
-                    result.append(splitted[0])
-                    result.append(splitted[1])
+        #go through pre_result line by line_numb
+        for line_numb in range(0,len(pre_result)):
+            #go through chacter k in the line
+            position = []
+            for k in range(0,len(pre_result[line_numb])-2):
+                if (pre_result[line_numb][k] == ',') and (pre_result[line_numb][k+2].isnumeric()):
+                    position.append(k)
                     continue
-                else:
-                    result.append(pre_result[n])
-                    continue
+            # print(position)
+
+            temp_list = list(pre_result[line_numb])
+
+            for p in range(0,len(position)):
+                temp_list.pop(position[p])
+                temp_list.insert(position[p], '\n')
+
+            temp_string = ''.join(temp_list)
+            splitted = temp_string.split('\n ')
+
+            for split in splitted:
+                result.append(split)
 
         # print(result)
 
@@ -54,12 +60,11 @@ for i in bible_title_chapter_list:
 
         final_result = []
         for k in new_result:
-            # print(k)
             final_result.append(k.lstrip('[').rstrip(']'))
-            # print(final_result)
+        # print(final_result)
 
-        file_path = '/Users/tj/PycharmProjects/bible_crawler/bible_text/{}/{}{}.text'
-        with open(file_path.format(i[0], i[0], i[j]), 'w') as fp:
+        file_path = '/Users/tj/PycharmProjects/bible_crawler/bible_text/{}. {}/{}{}.text'
+        with open(file_path.format(i[0], i[1], i[1], i[j]), 'w') as fp:
             fp.write('\n'.join(final_result))
 
         time.sleep(3)
